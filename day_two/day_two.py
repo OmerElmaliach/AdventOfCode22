d6e8cc58aadc3768f0d676c4d64b4fbbacbbfd9f
Omer Elmaliach <omerelmaliachprivate@gmail.com>
2022-12-03 14:55:10 +0200

    Day two part two added

--- day_two/day_two.py
+++ day_two/day_two.py
@@ -1,7 +1,7 @@
 
 
 def main():
-    print(part_one())
+    print(part_two())
 
 
 def part_one():
@@ -9,14 +9,14 @@ def part_one():
     with open('22D2.txt', 'r') as file:
         data = file.read().splitlines()
         for hand in data:
-            sum_score += calculate_win(hand)
+            sum_score += calculate_win_part_one(hand)
 
     return sum_score
 
 
-def calculate_win(hand):
+def calculate_win_part_one(hand):
     """
-    :param hand: The battle at play
+    :param hand: The hand at play
     :return: Score of player
     """
     opponent_hand, player_hand = hand.split()[0], hand.split()[1]
@@ -44,6 +44,48 @@ def calculate_win(hand):
         return 3 + 6
     elif player_hand == 'Z' and opponent_hand == 'C':
         return 3 + 3
+
+
+def part_two():
+    sum_score = 0
+    with open('22D2.txt', 'r') as file:
+        data = file.read().splitlines()
+        for hand in data:
+            sum_score += calculate_win_part_two(hand)
+
+    return sum_score
+
+
+def calculate_win_part_two(hand):
+    """
+    :param hand: The hand at play
+    :return: Score of player
+    """
+    opponent_hand, player_hand = hand.split()[0], hand.split()[1]
+
+    if player_hand == 'X':
+        if opponent_hand == 'A':
+            return 3 + 0
+        elif opponent_hand == 'B':
+            return 1 + 0
+        elif opponent_hand == 'C':
+            return 2 + 0
+
+    if player_hand == 'Y':
+        if opponent_hand == 'A':
+            return 1 + 3
+        elif opponent_hand == 'B':
+            return 2 + 3
+        elif opponent_hand == 'C':
+            return 3 + 3
+
+    if player_hand == 'Z':
+        if opponent_hand == 'A':
+            return 2 + 6
+        elif opponent_hand == 'B':
+            return 3 + 6
+        elif opponent_hand == 'C':
+            return 1 + 6
 
 
 if __name__ == '__main__':
