10741145a5afc0566639b7a6a83daffd8ba3075f
Omer Elmaliach <omerelmaliachprivate@gmail.com>
2022-12-11 18:58:57 +0200

    Flexibility && rewriting

--- day_nine/day_nine.py
+++ day_nine/day_nine.py
@@ -4,34 +4,51 @@ from math import sqrt
 def main():
     with open('22D9.txt', 'r') as file:
         data = file.read().split()
-        print(count_unique_lists(count_tail_positions(data)))
+        rope = setup_rope(10)
+        print(count_tail_pos(data, rope))
 
 
-def count_tail_positions(data):
-    head, loc_list = [0, 0], [[0, 0]]
+def setup_rope(num):
+    rope = []
+    for i in range(num):
+        rope.append([0, 0])
+
+    return rope
+
+
+def count_tail_pos(data, rope):
+    loc_lst = []
     for direction, times in zip(data[::2], data[1::2]):
-        if direction == 'U':
-            for i in range(int(times)):
-                head[1] += 1
-                if not is_in_range(head, loc_list[-1]):
-                    loc_list.append([head[0], head[1] - 1])
-        elif direction == 'D':
-            for i in range(int(times)):
-                head[1] -= 1
-                if not is_in_range(head, loc_list[-1]):
-                    loc_list.append([head[0], head[1] + 1])
-        elif direction == 'L':
-            for i in range(int(times)):
-                head[0] -= 1
-                if not is_in_range(head, loc_list[-1]):
-                    loc_list.append([head[0] + 1, head[1]])
-        elif direction == 'R':
-            for i in range(int(times)):
-                head[0] += 1
-                if not is_in_range(head, loc_list[-1]):
-                    loc_list.append([head[0] - 1, head[1]])
+        tail_pos = move_rope(rope, direction, times)
+        for loc in tail_pos:
+            if loc not in loc_lst:
+                loc_lst.append(loc)
 
-    return loc_list
+    return len(loc_lst)
+
+
+def move_rope(rope, direction, times):
+    tail_pos, current_pos, prev_head_pos = [0, 0], [0, 0], [0, 0]
+    for i in range(int(times)):
+        tail_pos.append(rope[-1])
+        for k in range(len(rope)):
+            if k == 0:
+                prev_head_pos = rope[0].copy()
+                if direction == 'U':
+                    rope[0][1] += 1
+                elif direction == 'D':
+                    rope[0][1] -= 1
+                elif direction == 'L':
+                    rope[0][0] -= 1
+                elif direction == 'R':
+                    rope[0][0] += 1
+
+            elif not is_in_range(rope[k - 1], rope[k]):
+                current_pos = rope[k]
+                rope[k] = prev_head_pos
+                prev_head_pos = current_pos
+
+    return tail_pos
 
 
 def is_in_range(pos1, pos2):
